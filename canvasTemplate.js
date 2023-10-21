bdbeefebc56957ea82fa4dc99aff774b7b8d002a
Mark Spronck <blindman67@users.noreply.github.com>
2023-10-22 01:18:16 +0800

    Update canvasTemplate.js

--- canvasTemplate.js
+++ canvasTemplate.js
@@ -1,6 +1,6 @@
 import {$,$$} from "./src/DOM/geeQry.js";
 
-function startApp(event) { location.href = event.target.dataset.url + "/index.html" }
+function startApp(event) { location.href = event.target.dataset.url.includes("index.html") ? event.target.dataset.url : (event.target.dataset.url + "/index.html"); }
 for (const link of $("?#root li")) { link.dataset.url && link.addEventListener("click", startApp) }
 
 
