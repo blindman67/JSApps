f8943ee7769c8b3d68ca171f6495c36c5b526b87
Mark Spronck <blindman67@users.noreply.github.com>
2023-11-08 11:31:27 +0800

    Resyled landing page.
    Still a lot to do until ready.

--- canvasTemplate.js
+++ canvasTemplate.js
@@ -8,28 +8,30 @@ matchMedia && (() => {
 (() => {
     const centreElement = el => (el.style.position = "absolute", el.style.left = ((innerWidth - el.getBoundingClientRect().width) * 0.5 | 0) + "px");
     centreElement(root);
-    function startApp(e) { location.href = e.target.dataset.url?.includes("index.html") ? e.target.dataset.url : e.target.dataset.named !== undefined ? e.target.dataset.named : (e.target.dataset.url + "/index.html"); }
-    for (const link of $("?#root li")) { (link.dataset.url || link.dataset.named) && link.addEventListener("click", startApp) }
-    for (const link of $("?#root > ul > li > span")) { (link.dataset.url || link.dataset.named) && link.addEventListener("click", startApp) }
-    
-    
-    var matchCount = 0, resetHdl;
-    document.addEventListener("keydown", (e) => {
-        if (pageTitle.dataset.key[matchCount] === e.key) {
-            matchCount ++;
-            if (matchCount === pageTitle.dataset.key.length) {
-                showCpp.style.display = "block";
-            } else {
-                clearTimeout(resetHdl);
-                resetHdl = setTimeout(() => { matchCount = 0; }, 1000);
-            }
+    function startApp(e) { 
+        if (e.target.dataset.target === "newTab") {
+            open(e.target.dataset.named !== undefined ? 
+                e.target.dataset.named : 
+                (e.target.dataset.url + "/index.html")
+                , "_blank");
         } else {
-            matchCount = 0;
+            location.href = e.target.dataset.url?.includes("index.html") ? 
+                e.target.dataset.url : 
+                e.target.dataset.named !== undefined ? 
+                    e.target.dataset.named : 
+                    (e.target.dataset.url + "/index.html"); 
         }
-            
-    });
+    }
+    for (const link of $("?#root li")) { 
+        (link.dataset.url || link.dataset.named) && link.addEventListener("click", startApp) 
+    }
+    for (const link of $("?#root > ul > li > span")) { 
+        (link.dataset.url || link.dataset.named) && link.addEventListener("click", startApp)
+    }
     
     
+
+  
 })();
 
 
